45d76f4de3500d6e71f4fbc695a7c53004e5f614
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2022-07-07 19:12:58 +0430

    fix: removed unused controller & service method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,11 +6,6 @@ import { AuthService } from './auth/auth.service';
 export class AppController {
   constructor(private readonly appService: AppService, private authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('/health-check')
   healthCheck(): string {
     return this.appService.healthCheck();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,7 @@
-import { Body, Injectable, Req, Res } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return JSON.stringify({
-      message: 'Hello World!',
-    });
-  }
-
   healthCheck(): string {
     return JSON.stringify({
       message: 'up',
