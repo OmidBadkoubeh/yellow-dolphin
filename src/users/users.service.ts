79edfc3b4bf69f39a914726647059ca0cc185227
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2022-08-04 13:08:03 +0430

    fix: users service issue in findOne

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+
+import { LoggerService } from '@/logger/logger.service';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/udpate-user.dto';
@@ -8,7 +10,10 @@ import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    private readonly logger: LoggerService = new Logger(UsersService.name),
+  ) {}
 
   async create(dto: CreateUserDto) {
     const user = await this.userModel.create(dto);
@@ -35,21 +40,23 @@ export class UsersService {
   }
 
   async findByPhoneNumber(phoneNumber: string) {
-    const found = await this.userModel.findOne({ where: { phoneNumber } });
+    const found = await this.userModel.findOne().where('phoneNumber').equals(phoneNumber);
     return found;
   }
 
   async findByEmail(email: string) {
-    const found = await this.userModel.findOne({ where: { email } });
+    const found = await this.userModel.findOne().where('email').equals(email);
     if (!found) {
+      this.logger.error(`User with email: ${email} was not found.`);
       throw new NotFoundException(`User with email: ${email} was not found.`);
     }
     return found;
   }
 
   private async findById(id: string) {
-    const found = await this.userModel.findOne({ where: { id } });
+    const found = await this.userModel.findById(id);
     if (!found) {
+      this.logger.error(`User with id: ${id} was not found.`);
       throw new NotFoundException(`User with id: ${id} was not found.`);
     }
     return found;
